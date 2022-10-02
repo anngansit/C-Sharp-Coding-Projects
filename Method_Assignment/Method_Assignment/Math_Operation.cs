6aea02cc51f92dfd7dde0f58c12fc2e16b11109f
anngansit <anngansit@yahoo.com>
2022-10-02 00:38:04 -0700

    Method_Assignment

---
+++ Method_Assignment/Method_Assignment/Math_Operation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    public class Math_Operation
+    {
+        public static int Add(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Sum = Num1 + Num2;
+            return Sum;
+
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Difference = Num1 - Num2;
+            return Difference;
+
+        }
+
+        public static Decimal Multiply(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            Decimal Product = Num1 * Num2;
+            return Product;
+
+        }
+
+        public static Double Divide(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            Double Quotient = (Double)Num1 / (Double)Num2;
+            return Quotient;
+        }
+    }
+
+}
+
+
---
+++ Method_Assignment/Method_Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter firt number:");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+
+            int Sum = Math_Operation.Add(Num1, Num2);
+            int Difference = Math_Operation.Subtract(Num1, Num2);
+            Decimal Product = Math_Operation.Multiply(Num1, Num2);
+            Double Quotient = Math_Operation.Divide(Num1, Num2);
+
+            Console.WriteLine("The sum is: " + Sum);
+            
+            Console.WriteLine("The difference is: " + Difference);
+
+            Console.WriteLine("The product is: " + Product);
+
+            Console.WriteLine("The quotient is: " + Quotient);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
