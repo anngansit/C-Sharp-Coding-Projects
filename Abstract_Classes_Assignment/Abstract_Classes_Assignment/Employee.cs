c185a4a5782c77a4e8e90a954537b8566c2220ac
anngansit <anngansit@yahoo.com>
2022-10-05 10:40:36 -0700

    Abstract_Classes_Assignment

---
+++ Abstract_Classes_Assignment/Abstract_Classes_Assignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    class Employee : Person
+    {
+        public new void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+
+
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -16,17 +16,22 @@ namespace TwentyOne
             //game.Play();
             //Console.ReadLine();
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob", };
+            game.ListPlayers();
             Console.ReadLine();
+
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
 
 
             //}
