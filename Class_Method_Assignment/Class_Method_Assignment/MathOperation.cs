8176bb38dbfccf84db8f6c0de36b018d4554dff0
anngansit <anngansit@yahoo.com>
2022-10-05 00:26:01 -0700

    Class_Method_Assignment

--- Class_Method_Assignment/Class_Method_Assignment/MathOperation.cs
+++ Class_Method_Assignment/Class_Method_Assignment/MathOperation.cs
@@ -10,20 +10,28 @@ namespace Class_Method_Assignment
     {
         public int X { get; set; }
         public int Y { get; set; }
-        
+
         public void Divide()
         {
             Console.WriteLine(Divide(X, Y));
             Console.ReadLine();
 
-                  }
+        }
 
         public static Double Divide(int X, int Y)
         {
-           
+
             Double Quotient = X / Y;
             return Quotient;
+            int Remainder = X % Y;
+            return Remainder;
 
+        }
+        public static Double Remainder(int X, int Y)
+        {
+
+            Double Remainder = X % Y;
+            return Remainder;
         }
     }
 }
--- Class_Method_Assignment/Class_Method_Assignment/Program.cs
+++ Class_Method_Assignment/Class_Method_Assignment/Program.cs
@@ -11,16 +11,19 @@ namespace Class_Method_Assignment
         public static void Main(string[] args)
         {
 
+            int Y = 2;
             MathOperation mathoperation = new MathOperation();
 
             Console.WriteLine("Enter a number:");
             int X = Convert.ToInt32(Console.ReadLine());
 
-            int Y = 2;
+           
             Double Quotient = MathOperation.Divide(X, Y);
+            Double Remainder = MathOperation.Remainder(X, Y);
           
             Console.WriteLine("The number you entered is: " + X);
             Console.WriteLine("The number you entered divided by 2 is: " + Quotient);
+            Console.WriteLine("Remainder: " + Remainder);
             Console.ReadLine();
         }
     }
