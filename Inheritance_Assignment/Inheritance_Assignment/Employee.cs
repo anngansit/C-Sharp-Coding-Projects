a95f5ac6f3ff988efe91ac3b55f40f3879f25bc3
anngansit <anngansit@yahoo.com>
2022-10-05 01:42:35 -0700

    Inheritance_Assignment

---
+++ Inheritance_Assignment/Inheritance_Assignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Assignment
+{
+    public class Employee : Person
+    {
+        public int ID;
+
+        public void IDList()
+            {
+            throw new NotImplementedException();
+            }
+
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,9 +10,15 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            //game.ListPlayers();
+            //game.Play();
+            //Console.ReadLine();
+
             Deck deck = new Deck();
             deck.Shuffle(3);
-           
+
 
             foreach (Card card in deck.Cards)
             {
@@ -21,20 +27,20 @@ namespace TwentyOne
 
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
-        
-       
-        //}
 
-        ////public static Deck Shuffle(Deck deck, int times)
-        ////{
-        ////    for (int i = 0; i < times; i++)
-        ////    {
-        ////        deck = Shuffle(deck);
-        ////    }
-        ////    return deck;
-        //}
 
- 
+            //}
+
+            ////public static Deck Shuffle(Deck deck, int times)
+            ////{
+            ////    for (int i = 0; i < times; i++)
+            ////    {
+            ////        deck = Shuffle(deck);
+            ////    }
+            ////    return deck;
+            //}
+
+
         }
     }
 }
