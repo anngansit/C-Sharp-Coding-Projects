5345b42d0708a42d2c3ff51ebc6e40b592475d30
anngansit <anngansit@yahoo.com>
2022-10-06 01:07:47 -0700

    Interfaces_Assignment

---
+++ Interfaces_Assignment/Interfaces_Assignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+    class Employee : Person, IQuittable
+    {
+        public new void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+           
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Employee " + firstName + " " + lastName + " is leaving the company.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interfaces_Assignment/Interfaces_Assignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void SayName()
+        {
+
+
+        }
+    }
+}
