50096ce24439ff0a3eded12d75fb7e969f23cfb8
anngansit <anngansit@yahoo.com>
2022-10-05 23:00:33 -0700

    Classes_and_Methods_Assign2

---
+++ Classess_and_Methods_Assign2/Classess_and_Methods_Assign2/MathOp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classess_and_Methods_Assign2
+{
+    class MathOp
+    {
+        public static int Output(int x, int y)
+        {
+            return x + y;
+            
+        }
+
+    }
+}
---
+++ Classess_and_Methods_Assign2/Classess_and_Methods_Assign2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classess_and_Methods_Assign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp mathOp = new MathOp();
+
+            int y = 10;
+            int Result;
+
+            Console.WriteLine("Enter an integer:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The second integer is: " + y);
+
+            Result = MathOp.Output(x, y);
+            
+            Console.WriteLine("The sum of the 2 numbers is: " + Result);
+            Console.ReadLine();
+        } 
+    }
+}
