e2cc2646cc1937ef0a14e69e6d39f6cb1a16817d
anngansit <anngansit@yahoo.com>
2022-11-07 21:47:28 -0800

    Datetime_Assignment

--- Datetime_Assignment/Datetime_Assignment/Program.cs
+++ Datetime_Assignment/Datetime_Assignment/Program.cs
@@ -12,7 +12,7 @@ namespace Datetime_Assignment
         public static void Main(string[] args)
             
         {
-            //DateTime d = new DateTime();
+           
             DateTime d1 = DateTime.Now;
             Console.WriteLine("Current time is:");
             Console.WriteLine(d1);
@@ -26,9 +26,6 @@ namespace Datetime_Assignment
 
             Console.ReadLine();
         }
-
-        //File.WriteAllText(@"C:\Anna\Logs\logs.txt", text);
-
     }
 }
   
