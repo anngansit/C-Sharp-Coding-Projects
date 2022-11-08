37a7cb01b85020a962c6eb4bbf0d338d027579c9
anngansit <anngansit@yahoo.com>
2022-11-07 21:46:22 -0800

    Datetime_Assignment

--- Datetime_Assignment/Datetime_Assignment/Program.cs
+++ Datetime_Assignment/Datetime_Assignment/Program.cs
@@ -7,21 +7,28 @@ using System.IO;
 
 namespace Datetime_Assignment
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            
         {
-            var date2 = DateTime.Now;
-            Console.WriteLine(date2);
-            
-            Console.WriteLine("\nPlease enter a number:");
-            string text = Console.ReadLine();
+            //DateTime d = new DateTime();
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine("Current time is:");
+            Console.WriteLine(d1);
+   
+            Console.WriteLine("\nPlease enter a number to add:");
+            double x = Convert.ToDouble(Console.ReadLine());
+          
+            DateTime d2 = d1.AddHours(x);
+            Console.WriteLine("Current time plus the number of hours you entered is:");
+            Console.WriteLine(d2);
 
-            //File.WriteAllText(@"C:\Anna\Logs\logs.txt", text);
-            Console.WriteLine("\n" + text + " hours");
-           
             Console.ReadLine();
-            
         }
+
+        //File.WriteAllText(@"C:\Anna\Logs\logs.txt", text);
+
     }
 }
+  
